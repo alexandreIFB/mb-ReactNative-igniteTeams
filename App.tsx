4a73e2b0d7ead053ff40fd96e1b5d178298fa877
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-09-20 21:43:28 -0300

    personalizando a status bar

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "styled-components";
 import theme from '@theme/index'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -12,6 +13,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
